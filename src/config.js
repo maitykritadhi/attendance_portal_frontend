a424e2942951171cd8653a042a969dfd84faceb2
maitykritadhi <maitykritadhi@gmail.com>
2023-09-19 23:23:07 +0530

    changed to PROD

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const BASEURL_DEV = `http://localhost:3000/`;
 const BASEURL_PROD = `https://attendance-portal-backend-lypv.onrender.com/`;
-// const ENV = `PROD`;
-const ENV = `DEV`;
+const ENV = `PROD`;
+// const ENV = `DEV`;
 
 module.exports = {
   BASEURL_DEV,
